e07f8ea20b551bb43df480c01a8ecdff0be6a2e4
Naoya Sawaguchi <naoya.sawaguchi@techplay.jp>
2025-08-20 16:59:09 +0900

    body parserの記述を改善

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 
 // Expressの設定
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 // ---- これより上は変更しないでください ----
 
 // クライアントからのリクエストを処理するコード
